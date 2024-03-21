416bafbb3745de561b847ce7c1b48555155b29f3
biiaa18 <splunch18@gmail.com>
2024-03-21 17:41:14 +0100

    Ready killable enemy
    Does not move, but can be killed with  shooting

---
+++ Source/Miniaturized/Public/NPC.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "NPC.generated.h"
+
+UCLASS()
+class MINIATURIZED_API ANPC : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ANPC();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	
+	/*Health*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy")
+	float health;
+
+	UFUNCTION(BlueprintCallable)
+	void TakeDamage(float damage);
+
+	UFUNCTION()
+	void Die();
+
+	FTimerHandle DieTimer;
+
+	UPROPERTY()
+	float DieDelay;
+};
