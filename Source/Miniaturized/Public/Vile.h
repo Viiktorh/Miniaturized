bfd494ead0d4ae51e1a2c82e7b6742528769c3b1
biiaa18 <splunch18@gmail.com>
2024-04-22 19:46:53 +0200

    Viles HUD added

--- Source/Miniaturized/Public/Vile.h
+++ Source/Miniaturized/Public/Vile.h
@@ -3,34 +3,29 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
 #include "Components/StaticMeshComponent.h"
-#include "Battery.generated.h"
+#include "GameFramework/Actor.h"
+#include "Vile.generated.h"
 
 UCLASS()
-class MINIATURIZED_API ABattery : public AActor
+class MINIATURIZED_API AVile : public AActor
 {
 	GENERATED_BODY()
-	
+
 public:	
+
 	// Sets default values for this actor's properties
-	ABattery();
+	AVile();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/*static mesh to represent the pickup in the level*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pickup")
+	class UStaticMeshComponent* VialMesh;
+
+	
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	
-
-	/*static mesh to represent the pickup in the level*/
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pickup")
-	UStaticMeshComponent* BatteryMesh;
-
-	
-
-
 };
