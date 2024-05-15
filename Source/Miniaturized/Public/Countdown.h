2790d02be4f87f6d317288ca80e60756e64a300f
biiaa18 <splunch18@gmail.com>
2024-05-15 15:07:23 +0200

    countdown added
    issues: destroys everything, including "ready mix" text
    currently starts only after player passes.
    does not restart/ restore collision  if character dies.

---
+++ Source/Miniaturized/Public/Countdown.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/TextRenderComponent.h"
+#include "Components/BoxComponent.h"
+#include "Countdown.generated.h"
+
+UCLASS()
+class MINIATURIZED_API ACountdown : public AActor
+{
+	GENERATED_BODY()
+	/* the inspiration for my code is: https://github.com/Taheri-meisam/March21CountDown.git */
+public:	
+	// Sets default values for this actor's properties
+	ACountdown();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void CountdownBegin();
+
+	UFUNCTION()
+	void CountdownFinished();
+
+	float Time = 60.0f;  // 120.0f
+
+	FTimerHandle CountdownTimer;
+
+	UTextRenderComponent* CountdownText = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	USoundBase* CountdownSound{ nullptr };
+
+	bool bCanPickupPotion;
+	bool bCollisionEnabled;
+
+	FString FormattedTime;
+
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* TriggerBoxTimer;
+
+	/*start timer*/
+
+	UFUNCTION()
+	void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	void OnBoxEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+
+
+
+};
